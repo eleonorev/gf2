614f0a2a093c4eea678dcd2078a4e559778cc7fd
Eleonore VOISIN--TRIBOULET <evoisin@e1r12p22.42.fr>
2016-05-05 17:18:33 +0200

    k

--- view/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DB_DSN = 'mysql:dbname=gifyou;host=localhost';
-$DB_USER = 'rootroot';
-$DB_PASSWORD = 'root';
-$connection = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
--- view/index.php
+++
@@ -1,8 +0,0 @@
-<?php include 'view/header.php';
-
-if (isset($_SESSION['login'])) {
-  include('view/profile.php');
-}
-include('view/wall.php');
-
- include 'view/footer.php'; ?>
