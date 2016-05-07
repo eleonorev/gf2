8d1e04b41da0733e85f7b0165b34680daca90d4a
Eleonore VOISIN--TRIBOULET <evoisin@e1r11p21.42.fr>
2016-05-07 19:57:28 +0200

    k

--- view/header.php
+++ view/header.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-ini_set("error_reporting", E_ALL);
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", E_ALL);
-include 'config/database.php';
+  session_start();
+  ini_set("error_reporting", E_ALL);
+  ini_set("display_errors", 1);
+  ini_set("display_startup_errors", E_ALL);
+  include 'config/database.php';
 ?>
 
 <!DOCTYPE html>
@@ -13,26 +13,28 @@ include 'config/database.php';
   <title> Gifyou</title>
   <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   <link rel="stylesheet" type="text/css" href="assets/css/nav.css">
+  <link href='https://fonts.googleapis.com/css?family=Mr+Dafoe|Dancing+Script|Marck+Script' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="assets/js/editor.js"></script>
 
 </head>
 <body>
-  <div id="header">
+<div id="header">
       <ul class="menu">
           <div class="menuright">
+            <span class="logo"> Gifyou </span>
                   </div>
 
                 </ul>
-  <div class="toggleicon" id="toggleicon">
-      <div class="top"></div>
-      <div class="middle"></div>
-      <div class="bottom"></div>
+  <div id="toggle" class="on">
+      <div class="one"></div>
+      <div class="two"></div>
+      <div class="three"></div>
   </div>
-    </div>
+</div>
 
-    <div id="navigation">
+    <div id="navigation" class="active">
         <?php
           if (isset($_SESSION['login'])) {
             include('navconnect.php');}
